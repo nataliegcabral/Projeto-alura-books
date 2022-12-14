740555f0da3eb5f60c3445959c66caa59714cd29
Natalie Cabral <nataliegc23@gmail.com>
2022-12-14 13:52:34 -0300

    update

---
+++ app/main.js
@@ -0,0 +1,12 @@
+let livros = []
+const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+getBuscarLivros()
+
+async function getBuscarLivros() {
+    const res = await fetch(endpointAPI)
+    livros = await res.json()
+    let  livrosComDesconto = aplicarDesconto(livros)
+
+    exibirLivros(livrosComDesconto)
+}
